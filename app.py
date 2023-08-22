979d64811b88b318017cabdef7d7bf7a864b2e87
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2023-08-22 15:15:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -45,11 +45,11 @@ def click_button():
             
 st.button('Rekomenasikan menit!', on_click=click_button)
 if st.session_state.clicked:
-    st.write('Untuk tim ini direkomendasikan memilih menit 1 hingga '+plotdata[1])
+    st.write('Untuk tim ini direkomendasikan memilih menit 1 hingga '+str(plotdata[1]))
         
 pn = plot_PN(pass_between, min_pass, filter, menit[0], menit[1], match)
         
-with open('/app/lb-passingnetwork/data/pnet.jpg', 'rb') as img:
+with open('pnet.jpg', 'rb') as img:
     fn = 'PN_'+filter+'-'+f_team+'.jpg'
     btn = st.download_button(label="Download Passing Network", data=img, file_name=fn, mime="image/jpg")
 st.pyplot(pn)
