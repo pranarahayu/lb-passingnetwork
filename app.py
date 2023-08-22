2e2aff524fb4b158f10c0cf5897270f404b45af8
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2023-08-22 15:11:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,41 +15,41 @@ with st.expander("BACA INI DULU."):
     st.write("")
     
 col1, col2 = st.columns(2)
-    with col1:
-        tl_data = st.file_uploader("Upload file timeline excel!")
-        tl = pd.read_excel(tl_data, skiprows=[0])
+with col1:
+    tl_data = st.file_uploader("Upload file timeline excel!")
+    tl = pd.read_excel(tl_data, skiprows=[0])
 
-    with col2:
-        rp_data = st.file_uploader("Upload file report excel!")
-        rp = pd.read_excel(rp_data, skiprows=[0])
-        team1 = df_m['Team'][0]
-        team2 = df_m['Opponent'][0]
-        match = team1 +' vs '+team2
+with col2:
+    rp_data = st.file_uploader("Upload file report excel!")
+    rp = pd.read_excel(rp_data, skiprows=[0])
+    team1 = df_m['Team'][0]
+    team2 = df_m['Opponent'][0]
+    match = team1 +' vs '+team2
             
 colx, coly, colz = st.columns(3)
-    with colx:
-        filter = st.selectbox('Select Team', [team1, team2])
-    with coly:
-        min_pass = st.number_input('Select Min. Successful Passes', min_value=0, max_value=3, step=1)
-    with colz:
-        menit = st.slider('Select Minutes', 0, 90, (1, 30))
+with colx:
+    filter = st.selectbox('Select Team', [team1, team2])
+with coly:
+    min_pass = st.number_input('Select Min. Successful Passes', min_value=0, max_value=3, step=1)
+with colz:
+    menit = st.slider('Select Minutes', 0, 90, (1, 30))
         
-    plotdata = get_PNdata(tl, rp, menit[0], menit[1], filter, min_pass)
-    pass_between = plotdata[0]
+plotdata = get_PNdata(tl, rp, menit[0], menit[1], filter, min_pass)
+pass_between = plotdata[0]
         
-    if 'clicked' not in st.session_state:
-        st.session_state.clicked = False
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
         
-    def click_button():
-        st.session_state.clicked = True
+def click_button():
+    st.session_state.clicked = True
             
-    st.button('Rekomenasikan menit!', on_click=click_button)
-    if st.session_state.clicked:
-        st.write('Untuk tim ini direkomendasikan memilih menit 1 hingga '+plotdata[1])
+st.button('Rekomenasikan menit!', on_click=click_button)
+if st.session_state.clicked:
+    st.write('Untuk tim ini direkomendasikan memilih menit 1 hingga '+plotdata[1])
         
-    pn = plot_PN(pass_between, min_pass, filter, menit[0], menit[1], match)
+pn = plot_PN(pass_between, min_pass, filter, menit[0], menit[1], match)
         
-    with open('/app/lb-passingnetwork/data/pnet.jpg', 'rb') as img:
-        fn = 'PN_'+filter+'-'+f_team+'.jpg'
-        btn = st.download_button(label="Download Passing Network", data=img, file_name=fn, mime="image/jpg")
-    st.pyplot(pn)
+with open('/app/lb-passingnetwork/data/pnet.jpg', 'rb') as img:
+    fn = 'PN_'+filter+'-'+f_team+'.jpg'
+    btn = st.download_button(label="Download Passing Network", data=img, file_name=fn, mime="image/jpg")
+st.pyplot(pn)
